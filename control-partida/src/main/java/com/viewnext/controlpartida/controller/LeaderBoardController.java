b534ad92537b08485b226bed5b9e624678567f85
vahaty <soniaramiro0@gmail.com>
2021-04-14 18:39:50 +0200

    controlPartida

--- control-partida/src/main/java/com/viewnext/controlpartida/controller/LeaderBoardController.java
+++ control-partida/src/main/java/com/viewnext/controlpartida/controller/LeaderBoardController.java
@@ -1,8 +1,32 @@
 package com.viewnext.controlpartida.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viewnext.controlpartida.domain.LeaderBoardRow;
+import com.viewnext.controlpartida.service.LeaderBoardService;
+
 /**
  * This class implements a REST API for the GameControl LeaderBoard service.
  */
+@RestController
+@RequestMapping("/lideres")
 class LeaderBoardController{
+	private final LeaderBoardService leaderBoardService;
 
+	@Autowired
+	public LeaderBoardController(final LeaderBoardService leaderBoardService) {
+		this.leaderBoardService = leaderBoardService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<LeaderBoardRow>> getRandomSuma() {
+		return ResponseEntity.ok(leaderBoardService.getCurrentLeaderBoard());
+
+	}
 }
--- control-partida/src/main/java/com/viewnext/controlpartida/controller/UserStatsController.java
+++ control-partida/src/main/java/com/viewnext/controlpartida/controller/UserStatsController.java
@@ -1,7 +1,30 @@
 package com.viewnext.controlpartida.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viewnext.controlpartida.domain.GameStats;
+import com.viewnext.controlpartida.service.GameService;
 /**
  * This class implements a REST API for the GameControl User Statistics service.
  */
+@RestController
+@RequestMapping("/estadisticas")
 class UserStatsController{
+	private final GameService gameService;
+
+	@Autowired
+	public UserStatsController(final GameService gameService) {
+		this.gameService = gameService;
+	}
+	
+	@GetMapping("/{resultId}")
+	public ResponseEntity<GameStats> getRSumaByID(final @PathVariable ("resultId") Long resultId) {
+		return ResponseEntity.ok(gameService.retrieveStatsForUser(resultId));
+
+	}
 }
