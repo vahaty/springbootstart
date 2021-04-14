c43f3e191a17653f7c81bce4702fedb47bcb88aa
vahaty <soniaramiro0@gmail.com>
2021-04-14 16:32:05 +0200

    calculadora

--- calculadora/src/main/java/com/viewnext/suma/controller/SumaController.java
+++ calculadora/src/main/java/com/viewnext/suma/controller/SumaController.java
@@ -1,9 +1,31 @@
 package com.viewnext.suma.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.viewnext.suma.domain.Suma;
+import com.viewnext.suma.service.SumaService;
+
+
 /**
  * This class implements a REST API for our Suma application.
  */
+@RestController
+@RequestMapping("/sumas")
+final class SumaController {
 
-final class SumaController{
-
+	private final SumaService sumaService;
+	
+	@Autowired
+	public SumaController(SumaService sumaService) {
+		this.sumaService = sumaService;
+	}
+	
+	@GetMapping("/random")
+	public Suma getRandomSuma() {
+		return sumaService.createRandomPlusOperation();
+	}
 }
